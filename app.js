643e9fe0dec98c2f8228effae267793c79ea161c
Bekzod <bekzodturgunoff@gmail.com>
2022-11-17 19:38:01 +0500

    testing the skills I have

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomInt(0, 3);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ hex.js
@@ -0,0 +1,18 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomInt(0, 15)];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
